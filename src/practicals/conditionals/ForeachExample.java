5495f8fcb25a160552226c9265b41c435a5aa7ba
Rishiraj Purohit <purohitji007@gmail.com>
2016-11-01 20:28:33 +0530

    foreach example added

---
+++ src/practicals/conditionals/ForeachExample.java
@@ -0,0 +1,10 @@
+package practicals.conditionals;
+
+public class ForeachExample {
+    public static void main(String[] args) {
+    	int[] numbers = {21,45,28,94,72,66};
+    	for (int number : numbers) {
+    		System.out.println(number);
+    	}
+    }
+}
---
+++ src/practicals/conditionals/Test.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public static void main(String[] args) {
+	System.out.print("hello from java");
+	System.out.println("hello snippet");
+	
+}
